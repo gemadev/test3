a7c3d53c3799eb337cdd066dc95db5972d56704f
Gonzalo <gonzalomartinezadalid@gmail.com>
2022-02-12 15:04:28 -0300

    test

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let check=document.querySelector(".check");
+check.addEventListener('click',language);
+
+function language(){
+    let id=check.checked;
+    if (id==true){
+        location.href="es/index.html";
+    }else{
+        location.href="../index.html"
+    }
+}
