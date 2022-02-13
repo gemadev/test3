69c59c24d6d64624fc747d5fc969de9740b7f836
Gonzalo <gonzalomartinezadalid@gmail.com>
2022-02-13 13:08:13 -0300

    final test

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-let check=document.querySelector(".check");
-check.addEventListener('click',language);
-
-function language(){
-    let id=check.checked;
-    if (id==true){
-        location.href="es/index.html";
+(() => {
+    const check = document.querySelector(".check");
+    check.addEventListener('click',language);
+    
+    const id = check.checked;
+    if (id == true){
+        document.getElementById('es-button').style.display = 'none';
+        document.getElementById('en-button').style.display = 'block';
     }else{
-        location.href="../index.html"
+        document.getElementById('en-button').style.display = 'none';
+        document.getElementById('es-button').style.display = 'block';
     }
-}
+
+    function language() {
+        let id = check.checked;
+        if (id === true){
+            location.href="es/index.html";
+        }else{
+            location.href="../index.html"
+        }
+    }
+})();
